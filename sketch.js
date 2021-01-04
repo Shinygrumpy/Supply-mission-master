5ff01b780175fc1bef92ff6e782cfbb7a5fe5943
Shinygrumpy <68494564+Shinygrumpy@users.noreply.github.com>
2021-01-04 08:17:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var dropzone1, dropzone2,dropzone3
+var body1,body2,body3	
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	dropzone1 = createSprite(width/2,665,200,20);
+	dropzone1.shapeColor=color(255,95,73);
+
+	dropzone2 = createSprite(300,620,20,100);
+	dropzone2.shapeColor=color(255,95,73);
+
+	dropzone3 = createSprite(500,620,20,100);
+	dropzone3.shapeColor=color(255,95,73);
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	body1 = Bodies.rectangle(width/2,665,200,20,{isStatic:true});
+	World.add(world,body1);
+
+	body2 = Bodies.rectangle(300,620,20,100,{isStatic:true});
+	World.add(world,body2);
+
+	body3 = Bodies.rectangle(500,620,20,100,{isStatic:true});
+	World.add(world,body3);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+    ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+
+    Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === DOWN_ARROW) {
+	   Matter.Body.setStatic(packageBody,false);
+	 }
+   }
+
+
+
